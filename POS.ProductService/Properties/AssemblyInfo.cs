48e6c8eb4ae2324e94d8390fb97111635beb730e
Chris Palmer <palmerc7@att.net>
2015-01-14 17:10:38 -0600

    Updated assembly info

--- POS.ProductService/Properties/AssemblyInfo.cs
+++ POS.ProductService/Properties/AssemblyInfo.cs
@@ -5,10 +5,10 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("POS.ProductService")]
+[assembly: AssemblyTitle("ProductService")]
 [assembly: AssemblyDescription("POS Product Service")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("GameStop Store Systems")]
+[assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("POS.ProductService")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
