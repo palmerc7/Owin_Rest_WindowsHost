79608e8215e74451d05ce8b21db0a61e2ea35046
Chris Palmer <palmerc7@att.net>
2015-01-28 13:24:47 -0600

    Adding shared data project.  Adding a few get methods on ProductController

---
+++ Owin.POC.Data/Product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Owin.POC.Data
+{
+    public class Product
+    {
+        public string ProductId { get; set; }
+        public string Status { get; set; }
+        public string Name { get; set; }
+        public string Sku { get; set; }
+        public decimal RetailPrice { get; set; }
+        public int QtyInStock { get; set; }
+    }
+}
--- Owin.POC/Controllers/ProductController.cs
+++ Owin.POC/Controllers/ProductController.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using System.Web.Http.Description;
+
+using Owin.POC.Data;
 
 namespace Owin.POC.Controllers
 {
@@ -21,6 +24,64 @@ namespace Owin.POC.Controllers
             {
                 Content = new StringContent(string.Format("Validate SUCCESS from Product API: {0}", DateTime.Now.ToString()))
             };
+        }
+
+        [HttpGet]
+        [Route("getProductById")]
+        [ResponseType(typeof(Product))]
+        public IHttpActionResult GetProductById(string productId)
+        {
+            if (string.IsNullOrEmpty(productId))
+                return NotFound();
+
+            var product = new Product();
+            try
+            {
+                // Just returning fake data
+                product = new Product
+                {
+                    ProductId = productId,
+                    Name = "Test Product",
+                    QtyInStock = 11,
+                    RetailPrice = 1.99m,
+                    Sku = "1001",
+                    Status = "Active"
+                };
+            }
+            catch
+            {
+            }
+
+            return Ok(product);
+        }
+
+        [HttpGet]
+        [Route("searchProduct")]
+        [ResponseType(typeof(Product))]
+        public IHttpActionResult SearchProduct(string searchCriteria)
+        {
+            if (string.IsNullOrEmpty(searchCriteria))
+                return NotFound();
+
+            var product = new Product();
+            try
+            {
+                // Just returning fake data
+                product = new Product
+                {
+                    ProductId = "1",
+                    Name = "Test Product Search",
+                    QtyInStock = 12,
+                    RetailPrice = 1.88m,
+                    Sku = "1001",
+                    Status = "InActive"
+                };
+            }
+            catch
+            {
+            }
+
+            return Ok(product);
         }
 
     }
